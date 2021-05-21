c8f85fe2351dd3d8f1b1b362917d24c4a1a02610
LuKezLima <lucas>
2021-05-21 00:17:42 -0300

    done

---
+++ README.md
@@ -0,0 +1,46 @@
+<div align="center" id="top"> 
+  <img src="./.github/app.gif" alt="DecToBin" />
+
+  &#xa0;
+
+  <!-- <a href="https://dectobin.netlify.app">Demo</a> -->
+</div>
+
+<h1 align="center">DecToBin</h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/{{YOUR_GITHUB_USERNAME}}/dectobin?color=56BEB8">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/{{YOUR_GITHUB_USERNAME}}/dectobin?color=56BEB8">
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/{{YOUR_GITHUB_USERNAME}}/dectobin?color=56BEB8">
+
+  <img alt="License" src="https://img.shields.io/github/license/{{YOUR_GITHUB_USERNAME}}/dectobin?color=56BEB8">
+
+  <!-- <img alt="Github issues" src="https://img.shields.io/github/issues/{{YOUR_GITHUB_USERNAME}}/dectobin?color=56BEB8" /> -->
+
+  <!-- <img alt="Github forks" src="https://img.shields.io/github/forks/{{YOUR_GITHUB_USERNAME}}/dectobin?color=56BEB8" /> -->
+
+  <!-- <img alt="Github stars" src="https://img.shields.io/github/stars/{{YOUR_GITHUB_USERNAME}}/dectobin?color=56BEB8" /> -->
+</p>
+
+<!-- Status -->
+
+<!-- <h4 align="center"> 
+	🚧  DecToBin 🚀 Under construction...  🚧
+</h4> 
+
+<hr> -->
+
+<p align="center">
+  <img src="./back.png">
+</p>
+
+<br>
+
+## :dart: About ##
+
+Conversor Decimal to Binary
+
+
+<a href="#top">Back to top</a>
--- conversor.js
+++ conversor.js
@@ -1,49 +1,41 @@
+const button = document.getElementById("button");
+const text = document.getElementById("decimal");
+const container = document.getElementById("result");
+const h2 = document.createElement("h2");
 
-const button = document.getElementById('button')
-const text = document.getElementById('decimal').value;
-const container = document.getElementById('result')
-const h2 = document.createElement('h2')
+function conversor(decimal) {
+  let binario = 0;
+  let dec = decimal;
+  let array2 = [];
 
-
-
-function conversor(decimal){
-  console.log(Number(text));
- 
-  
-  
-  
-  
-  let dec = decimal
-  let array2 = []
-
-  while (decimal >= 2) {
-    let sobras = decimal % 2
-    decimal = Math.floor(decimal/2)
-      if(decimal == 1){
-        array2.push(sobras)
-        array2.push(1)
-      } else{
-        array2.push(sobras)
-      }
-  
+  if (decimal == 0) {
+    binario = 0;
   }
-  array2.reverse()
+  if (decimal == 1) {
+    binario = 1;
+  }
+  if(decimal > 1 ){
+    while (decimal >= 2) {
+      let sobras = decimal % 2;
+      decimal = Math.floor(decimal / 2);
+      if (decimal == 1) {
+        array2.push(sobras);
+        array2.push(1);
+      } else {
+        array2.push(sobras);
+      }
+      
+    }
+    array2.reverse();
+    binario = array2.join("");
+  }
 
-  let binario = array2.join('')
- 
-  
+  h2.innerText = `O número ${dec} em binário é ${binario}`;
 
-  h2.innerText = `O número ${dec} em binário é ${binario}`
-  
-  container.appendChild(h2)
+  container.appendChild(h2);
+}
 
-  
- }
-
- button.addEventListener('click',(e)=>{
-  e.preventDefault()
-  conversor(text)
- }
- )
-
-
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  conversor(text.value);
+});
